e60d2cd68504ee4f2dfaaf8c27dd18de150396e8
Teh Zhi XIan <84586784+Nuggetierer@users.noreply.github.com>
2022-04-19 15:20:41 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,9 +11,12 @@ public class Question2
      */
      
     Scanner in1 = new Scanner(System.in);
+    System.out.println("Enter Height: ");
     double height = in1.nextDouble();
+    height = height/10
     
     Scanner in2 = new Scanner(System.in);
+    System.out.println("Enter Weight: ");
     double weight = in2.nextDouble();
     
     System.out.println(weight / (height  * height));
