b3ec2af93bb30b4eb88a5e5e77715aea6fab3a43
Teh Zhi XIan <84586784+Nuggetierer@users.noreply.github.com>
2022-04-20 09:41:34 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -27,6 +27,33 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int val = in.nextInt();
+    ArrayList<Integer> list = new ArrayList<Integer>();
+
+    for (int i = val; i > 0; i--) {
+      int to_list = in.nextInt();
+      list.add(to_list);
+    }
+    /* Holder */
+    ArrayList<Integer> val_hold = new ArrayList<Integer>();
+    val_hold.add(0);
+    val_hold.add(0);
+
+    for (int i = 0; i < list.size(); i++) {
+      int counter = 0;
+      int check = list.get(i);
+      for (int x = 0; x < list.size(); x++) {
+        if (list.get(x) == check) {
+          counter += 1;
+        }
+      }
+      if (val_hold.get(1) < counter)
+      {
+        val_hold.set(0, check);
+        val_hold.set(1, counter);
+      }
+    }
+
+    System.out.println(val_hold.get(0));
   }
 }
